34d2b364147fb0287031a5769dcefd9776a2f54e
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-01-12 21:44:21 -0600

    quiz is functional with pages working, but not timed and does not start on start button

--- script.js
+++ script.js
@@ -1,46 +1,141 @@
- var startBtn = document.querySelector("#startbtn");
- startBtn.addEventListener("click", function(){
-    //  start the timer
-    var timeLeft = 59
-    var timer = setInterval(function(){
-        document.getElementById("counter").innerHTML = timeLeft + " seconds remaining";
-        timeLeft = timeLeft - 1;
-        if(timeLeft <=0) {
-            clearInterval(timer);
-            document.getElementById("counter").innerHTML = "Game over"
-        }
-    },1000);
-startBtn.addEventListener("click", function(){
-    var questionOptions = [{
-        question: "Question text here",
-        answers: ["answer1","answer2","answer3","answer4"],
-        correctAnswer: 0,
-    }, {
-        question: "Question text here",
-        answers: ["answer1","answer2","answer3","answer4"],
-        correctAnswer: 1,
-    }, {
-        question: "Question text here",
-        answers: ["answer1","answer2","answer3","answer4"],
-        correctAnswer: 2,
-    }];
-    
-})
-    
-    // everything in this section needs to go in a while loop that means it will all run while the timer still has time left 
-    // fill in the content of each question 
-    // first question will kick off by start button
-    // each subsequent question is kicked off by answering the previous question
-    // fill in question content in the id "questionText"
-    // fill in the answers in the id "answer#" and indicate which is correct 
-    // create an object with the question text, answer choices, and correct answer choice
-
-
-
-
- });
-
-
-
-
+//  var startBtn = document.querySelector("#startbtn");
  
+//  startBtn.addEventListener("click", function(){
+//     //  start the timer
+//     var timeLeft = 59;
+//     var timer = setInterval(function(){
+//         document.getElementById("counter").innerHTML = timeLeft + " seconds remaining";
+//         timeLeft =- 1;
+//         if(timeLeft <=0) {
+//             clearInterval(timer);
+//             document.getElementById("counter").innerHTML = "Game over"
+//         }
+//     },1000);
+//  );
+
+function quizDisplay(){
+
+    var out = [];
+
+    questionChoice.forEach(function(currentQuestion, questionNum){
+        var answers = [];
+
+        for(letter in currentQuestion.answers){
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNum}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
+        }
+
+        out.push(
+            `<div class="slide">
+            <div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join("")} </div>
+          </div>`
+        );
+      }
+    );
+    quizContainer.innerHTML = out.join('');
+  }
+
+
+  function showResults(){
+    var answerContainers = quizContainer.querySelectorAll('.answers');
+    let numCorrect = 0;
+
+    questionChoice.forEach( function(currentQuestion, questionNum) {
+      var answerContainer = answerContainers[questionNum];
+      var selector = `input[name=question${questionNum}]:checked`;
+      var userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  
+      if(userAnswer === currentQuestion.correctAnswer){
+        numCorrect++;
+      }
+      else{
+        // add a function that decreases the amount of time available - STILL NEED TO DO
+      }
+    });
+
+    resultsContainer.innerHTML = `${numCorrect} out of ${questionChoice.length}`;
+  }
+
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    
+    if(currentSlide === 0){
+      prevButton.style.display = 'none';
+    }
+    else{
+      prevButton.style.display = 'inline-block';
+    }
+    
+    if(currentSlide === slides.length-1){
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
+    }
+    else{
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
+    }
+  }
+
+function showNextSlide() {
+    showSlide(currentSlide + 1);
+}
+  
+function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+}
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+var questionChoice = [
+    {
+      question: "Question One Text",
+      answers: {
+        a: "answerone",
+        b: "answertwo",
+        c: "answerthree"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Question Two Text",
+      answers: {
+        a: "answerone",
+        b: "answertwo",
+        c: "answerthree"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which tool can you use to ensure code quality?",
+      answers: {
+        a: "answerone",
+        b: "answertwo",
+        c: "answerthree"
+      },
+      correctAnswer: "a"
+    }
+  ];
+
+
+quizDisplay();
+
+var prevButton = document.getElementById("prev");
+var nextButton = document.getElementById("next");
+var slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+showSlide(currentSlide);
+
+submitButton.addEventListener('click', showResults);
+prevButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
