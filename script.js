107290ccbe00867ee2d635c1a7240a1c46e783ef
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-01-14 22:35:05 -0600

    added comments to script file

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+// create the start button variable
 var startBtn = document.querySelector("#startbtn");
 
+// add variables globally so they can be used in other functions
 var timeLeft = 100;
 var timer;
 
+// start timer on Start Button click
 startBtn.addEventListener("click", function(){
-  //  start the timer
   timer = setInterval(function(){
       document.getElementById("timer").innerHTML = timeLeft + " seconds remaining";
       timeLeft -= 1;
@@ -16,17 +18,21 @@ startBtn.addEventListener("click", function(){
   },1000)}
 );
 
+// show the quiz container when you click the start button
 startBtn.addEventListener("click", function(){
   document.getElementById("quizContainer").style.display = "block";
 })
 
+// function entireQuiz contains all functions related to showing quiz questions, deciding on correct answer, and ending quiz
 function entireQuiz(){
+  // first function: display the quiz questions 
   function quizDisplay(){
     var out = [];
     questionChoice.forEach(function(currentQuestion, questionNum){
         var answers = [];
         for(letter in currentQuestion.answers){
           answers.push(
+            // use template literal to create question and answers display after a radio button
             `<label>
               <input type="radio" name="question${questionNum}" value="${letter}">
               ${letter} :
@@ -45,6 +51,7 @@ function entireQuiz(){
     quizContainer.innerHTML = out.join('');
   }
 
+  // second function: decide if the user has selected the correct answer and display what their score is
   function showResults(){
     var answerContainers = quizContainer.querySelectorAll('.answers');
     let numCorrect = 0;
@@ -71,7 +78,8 @@ function entireQuiz(){
     
   }
 
-function showcard(n) {
+  // third function: allows only one question to be displayed at a time 
+  function showcard(n) {
     cards[currentcard].classList.remove('active-card');
     cards[n].classList.add('active-card');
     currentcard = n;
@@ -93,19 +101,22 @@ function showcard(n) {
     }
   }
 
-function showNextcard() {
+  // the next two funtions enable the next and previous buttons to actually show the next and previous questions
+  function showNextcard() {
     showcard(currentcard + 1);
-}
+  }
   
-function showPreviouscard() {
+  function showPreviouscard() {
     showcard(currentcard - 1);
-}
+  }
 
+// create variables for the main content containers and the submit button
 var quizContainer = document.getElementById('quiz');
 var resultsContainer = document.getElementById('counter');
 var submitButton = document.getElementById('submit');
 
 
+// array of questions and answers
 var questionChoice = [
     {
       question: "Question One Text",
@@ -136,15 +147,19 @@ var questionChoice = [
     }
   ];
 
+// call the overall quiz function
 quizDisplay();
 
+// create the variables for the previous and next buttons and card/current card
 var prevButton = document.getElementById("prev");
 var nextButton = document.getElementById("next");
 var cards = document.querySelectorAll(".card");
 let currentcard = 0;
 
+// call function to show the correct carad
 showcard(currentcard);
 
+//  click events for the submit and previous/next buttons
 submitButton.addEventListener('click', showResults);
 prevButton.addEventListener("click", showPreviouscard);
 nextButton.addEventListener("click", showNextcard);
@@ -153,18 +168,25 @@ nextButton.addEventListener("click", showNextcard);
 
 
  
+// kicks off the entire quiz
 startBtn.addEventListener("click", entireQuiz)
 
 var submitButton = document.getElementById('submit');
 
+// final step: show the form to log high scores when the user clicks "submit button"
 submitButton.addEventListener("click", function(){
   document.getElementById("form").style.display = "block"
   document.getElementById("quizContainer").style.display = "none"
-  // var score = (numCorrect/questionChoice.length)*100;
   var scoreContainer = document.getElementById('score');
   scoreContainer.innerHTML = score.value;
   console.log(scoreContainer);
 })
+
+
+// still need to add function to save high score to local storage
+// still need to add funtion that writes highest score to the html
+// still need to add function that stops the quiz if the time runs out
+// still need to add function that subtracts time from quiz if user gets question wrong
 
 
 
